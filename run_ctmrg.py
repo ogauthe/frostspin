fd2dcb8232a7c55a33e1960fd384751f914d099c
Olivier Gauthé <olivier.gauthe@irsamc.ups-tlse.fr>
2020-04-30 12:28:22 +0200

    more tests in run

--- run_ctmrg.py
+++ run_ctmrg.py
@@ -41,15 +41,28 @@ tAKLT2[3,1,1,1,0] = 0.5
 tAKLT2[4,1,1,1,1] = 1.0
 
 gen5 = construct_genSU2_s(2)
-SdS55 = np.tensordot(gen5, -gen5.conj(), (0,0)).real.swapaxes(1,2).reshape(25,25)
+cconj5 = np.rint(lg.expm(-1j*np.pi*gen5[1]).real)  # SU(2) charge conjugation = pi-rotation over y
+tAKLT2_conj = np.tensordot(cconj5, tAKLT2,((1,),(0,)))
+SdS55 = np.tensordot(gen5, gen5, (0,0)).real.swapaxes(1,2).reshape(25,25)
 SdS55_2 = SdS55 @ SdS55
+SdS55b = np.tensordot(gen5, -gen5.conj(), (0,0)).real.swapaxes(1,2).reshape(25,25)
+SdS55b_2 = SdS55b @ SdS55b
 
 H_AKLT = 1/28*SdS55 + 1/40*SdS55_2 + 1/180*SdS55@SdS55_2 + 1/2520*SdS55_2@SdS55_2
+H_AKLT_55b = 1/28*SdS55b + 1/40*SdS55b_2 + 1/180*SdS55b@SdS55b_2 + 1/2520*SdS55b_2@SdS55b_2
 ###############################################################################
+
+
+print('#'*79,'\nTest asymetric CTMRG')
 
 d = 5
 D = 2
 chi = 3
+
+tensors = [tAKLT2.copy()]
+tiling = """
+A
+"""
 
 tensors = [tAKLT2.copy(),tAKLT2.copy(),tAKLT2.copy()]
 tiling = """
@@ -58,13 +71,14 @@ BCA
 CAB
 """
 
-tensors = [tAKLT2.copy()]
+tensors = [tAKLT2.copy(),tAKLT2_conj.copy()]
 tiling = """
-A
+AB
+BA
 """
 
 
-ctm = CTMRG(tensors,tiling,chi,verbosity=100)
+ctm = CTMRG(tensors,tiling,chi,verbosity=0)
 print('CTM constructed, tensors shapes are:')
 print('T1 shape =', ctm.env.get_T1(0,0).shape)
 print('C1 shape =', ctm.env.get_C1(0,0).shape)
@@ -78,7 +92,7 @@ print('C4 shape =', ctm.env.get_C4(0,0).shape)
 print('\ntry 2 iterations:')
 ctm.iterate()
 ctm.iterate()
-ctm.chi = 5
+ctm.chi = 10
 ctm.iterate()
 print('done')
 
@@ -91,10 +105,54 @@ rdm1x2 = ctm.compute_rdm1x2()
 rdm2x1 = ctm.compute_rdm2x1()
 rdm2x2 = ctm.compute_rdm2x2()
 
-print('rdm1x1 is hermitian:', lg.norm(rdm1x1-rdm1x1.T.conj()))
-print('rdm1x2 is hermitian:', lg.norm(rdm1x2-rdm1x2.T.conj()))
-print('rdm2x1 is hermitian:', lg.norm(rdm2x1-rdm2x1.T.conj()))
-print('rdm2x2 is hermitian:', lg.norm(rdm2x2-rdm2x2.T.conj()))
+print(f'rdm1x1 is hermitian: {lg.norm(rdm1x1-rdm1x1.T.conj()):.1e}')
+print(f'rdm1x2 is hermitian: {lg.norm(rdm1x2-rdm1x2.T.conj()):.1e}')
+print(f'rdm2x1 is hermitian: {lg.norm(rdm2x1-rdm2x1.T.conj()):.1e}')
+print(f'rdm2x2 is hermitian: {lg.norm(rdm2x2-rdm2x2.T.conj()):.1e}')
 
-print('trace(H_AKLT @ rdm1x2) =', np.trace(H_AKLT @ rdm1x2))
-print('trace(H_AKLT @ rdm2x1) =', np.trace(H_AKLT @ rdm2x1))
+print(f'trace(H_AKLT_55b @ rdm1x2) = {np.trace(H_AKLT_55b @ rdm1x2):.1e}')
+print(f'trace(H_AKLT_55b @ rdm2x1) = {np.trace(H_AKLT_55b @ rdm2x1):.1e}')
+print(f'trace(H_AKLT @ rdm1x2) = {np.trace(H_AKLT @ rdm1x2):.1e}')
+print(f'trace(H_AKLT @ rdm2x1) = {np.trace(H_AKLT @ rdm2x1):.1e}')
+
+T1s = ctm._env._neq_T1s
+print('T1s are all the same:', end=' ')
+for t in T1s[1:]:
+  print(f'{lg.norm(t-T1s[0]):.1e}', end=', ')
+
+T2s = ctm._env._neq_T2s
+print('\nT2s are all the same:', end=' ')
+for t in T2s[1:]:
+  print(f'{lg.norm(t-T2s[0]):.1e}', end=', ')
+
+T3s = ctm._env._neq_T3s
+print('\nT3s are all the same:', end=' ')
+for t in T3s[1:]:
+  print(f'{lg.norm(t-T3s[0]):.1e}', end=', ')
+
+T4s = ctm._env._neq_T4s
+print('\nT4s are all the same:', end=' ')
+for t in T4s[1:]:
+  print(f'{lg.norm(t-T4s[0]):.1e}', end=', ')
+
+C1s = ctm._env._neq_C1s
+print('\nC1s are all the same:', end=' ')
+for t in C1s[1:]:
+  print(f'{lg.norm(t-C1s[0]):.1e}', end=', ')
+
+C2s = ctm._env._neq_C2s
+print('\nC2s are all the same:', end=' ')
+for t in C2s[1:]:
+  print(f'{lg.norm(t-C2s[0]):.1e}', end=', ')
+
+C3s = ctm._env._neq_C3s
+print('\nC3s are all the same:', end=' ')
+for t in C3s[1:]:
+  print(f'{lg.norm(t-C3s[0]):.1e}', end=', ')
+
+C4s = ctm._env._neq_C4s
+print('\nC4s are all the same:', end=' ')
+for t in C4s[1:]:
+  print(f'{lg.norm(t-C4s[0]):.1e}', end=', ')
+
+print('\n'+'#'*79)
