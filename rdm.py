c87457100e266cffd33f57325cd74b7ff1abda9b
Olivier Gauthé <olivier.gauthe@irsamc.ups-tlse.fr>
2020-04-30 20:10:10 +0200

    bugless rdm1x2 and rdm2x1

--- rdm.py
+++ rdm.py
@@ -80,11 +80,13 @@ def rdm_1x2(env,x,y):
   C1 = env.get_C1(x,y)
   d = Al.shape[0]
   D = Al.shape[3]
+  D2 = D**2
   chi = C1.shape[0]
+  l = D2*chi
   T1l = env.get_T1(x+1,y)
   D2 = T1l.shape[1]
   T1r = env.get_T1(x+2,y)
-  T1r = np.dot(env.get_C2(x+3,y),T1r.reshape(chi,D**2*chi))  # include C2 here
+  C2 = env.get_C2(x+3,y)
   T2 = env.get_T2(x+3,y+1)
   C3 = env.get_C3(x+3,y+2)
   T3r = env.get_T3(x+2,y+2)
@@ -92,9 +94,9 @@ def rdm_1x2(env,x,y):
   C4 = env.get_C4(x,y+2)
   T4 = env.get_T4(x,y+1)
 
-  left = np.dot(C1,T4.reshape(chi,D2*chi)).reshape(chi*D2, chi)
-  left = np.dot(left,C4).reshape(chi*D2, chi)
-  left = np.dot(left,T3l.transpose(2, 0, 1).reshape(chi, D2*chi)).reshape(chi, D, D, D, D, chi)
+  left = np.dot(C1,T4.reshape(chi,l)).reshape(l, chi)
+  left = np.dot(left,C4).reshape(l, chi)
+  left = np.dot(left,T3l.transpose(2, 0, 1).reshape(chi, l)).reshape(chi, D, D, D, D, chi)
   left = left.transpose(3, 1, 0, 5, 4, 2).reshape(D2,chi**2*D2)
   #   L-0            L-0
   #   L              L
@@ -102,39 +104,40 @@ def rdm_1x2(env,x,y):
   #   L              L
   #   L  34          L  34
   #   LLLLL-5        LLLLLL-5
-  left = np.dot(Al.reshape(d*D2,D2),left).reshape(d*D2*chi**2, D2)
-  left  = np.dot(left,Al.conj().transpose(3, 4, 0, 1, 2).reshape(D2, D2*d)).reshape(d,D,D,chi,chi,d,D,D)
+  left = np.dot(Al.reshape(d*D2,D2),left).reshape(d*l*chi, D2)
+  left  = np.dot(left,Al.conj().transpose(3, 4, 0, 1, 2).reshape(D2, D2*d)).reshape(d,D,D,chi,chi*d,D,D)
   #     L-3               L-2
   #   0 L 16            3 L 01
   #    \LLLL=27   =>     \LLLL=47
   #    /L                /L
   #   5 L-4             6 L-5
-  left = left.transpose(1,6,3,0,2,4,5,7).reshape(D2*chi,d**2*chi*D2)
+  left = left.transpose(1,5,3,0,2,4,6).reshape(l,d**2*l)
   left = np.dot(T1l.reshape(chi,D2*chi),left).reshape(chi, d, D, chi, d, D)
   right = np.dot(C3.T,T2.swapaxes(0,1).reshape(chi, D2*chi))
   #   1           0
   #  2R           ||
   #  0R         2-T3r-1
-  right = np.dot(T3r.swapaxes(1,2).reshape(D**2*chi,chi), right).reshape(D, D, chi, chi, D, D)
-  right = right.transpose(0, 4, 2, 3, 5, 1).reshape(D2,chi**2*D2)
+  right = np.dot(T3r.swapaxes(1,2).reshape(l,chi), right).reshape(D, D, chi**2, D, D)
+  right = right.transpose(0, 3, 2, 4, 1).reshape(D2,chi*l)
   #          3    0                   3    0     2
   #         4R     \1                1R     \1    \3
   #         5R     4A2      =>       4R     2A4   4A*0
   #      01  R      3              05 R      3     1
   #    2-RRRRR                   2RRRRR
-  right = np.dot(Ar.swapaxes(2,4).reshape(D2*d, D2), right).reshape(d*D2*chi**2,D2)
+  right = np.dot(Ar.swapaxes(2,4).reshape(D2*d, D2), right).reshape(d*l*chi,D2)
   right = np.dot(right,Ar.conj().transpose(2, 3, 0, 1, 4).reshape(D2, d*D2)).reshape(d,D,D,chi,chi,d,D,D)
   #           4 0                 5 0
   #        16 R/               67 R/
   #   2,7=RRRRR\    =>    3,4=RRRRR\
   #           R 5                 R 1
   #      3RRRRR              2RRRRR
-  right = right.transpose(0,5,3,2,7,4,1,6).reshape(D2*chi*d**2, D2*chi)
-  right = np.dot(right,T1r.reshape(chi*D2,chi)).reshape(d**2, D2*chi**2)
+  right = right.transpose(0,5,3,2,7,4,1,6).reshape(l*d**2, l)
+  T1r_ = np.dot(C2,T1r.reshape(chi,l))
+  right = np.dot(right,T1r_.reshape(l,chi)).reshape(d**2, l*chi)
   #     L-0           L-3        5-R
   # 1,4=L=2,5  => 3,4=L=1,2    3,4=R=0,1
   #     L-3           L-0        2-R
-  left = left.transpose(3,2,5,0,1,4).reshape(D2*chi**2,d**2)
+  left = left.transpose(3,2,5,0,1,4).reshape(l*chi,d**2)
   rdm = np.dot(right,left).reshape(d, d, d, d)
   del right,left
   rdm = rdm.transpose(2,0,3,1).reshape(d**2,d**2)
@@ -170,57 +173,47 @@ def rdm_2x1(env,x,y):
   Au = env.get_A(x+1,y+1)
   Ad = env.get_A(x+1,y+2)
   C1 = env.get_C1(x,y)
-  d = Au.shape[0]
-  D = Au.shape[1]
-  chi = C1.shape[0]
   T1 = env.get_T1(x+1,y)
-  T1 = T1.reshape(T1.shape[0],D,D,T1.shape[2])
   C2 = env.get_C2(x+2,y)
   T2u = env.get_T2(x+2,y+1)
-  T2u = T2u.reshape(T2u.shape[0],T2u.shape[1],D,D)
   T2d = env.get_T2(x+2,y+2)
-  T2d = T2d.reshape(T2d.shape[0],T2d.shape[1],D,D)
   C3 = env.get_C3(x+2,y+3)
   T3 = env.get_T3(x+1,y+3)
-  T3 = T3.reshape(D,D,T3.shape[1],T3.shape[2])
   C4 = env.get_C4(x,y+3)
   T4d = env.get_T4(x,y+2)
-  T4d = T4d.reshape(T4d.shape[0],D,D,T4d.shape[2])
   T4u = env.get_T4(x,y+1)
-  T4u = T4u.reshape(T4u.shape[0],D,D,T4u.shape[2])
+  d = Au.shape[0]
+  D = Au.shape[1]
+  D2 = D**2
+  chi = C1.shape[0]
+  l = chi*D2
+  Au = Au.transpose(1, 2, 0, 3, 4).reshape(D2, D2*d)
+  Ad = Ad.transpose(3, 4, 0, 1, 2).reshape(D2, D2*d)
 
-  up = np.dot(C2,T1.reshape(chi, D**2*chi)).reshape(chi, D, D, chi)
-  down = T3.transpose(0, 1, 3, 2).reshape(D**2*chi, chi)
-  down = np.dot(down,C3.T).reshape(D, D, chi, chi)
-  down = down.transpose(0, 1, 3, 2).reshape(D**2*chi, chi)
-  down = np.dot(down,C4.T).reshape(D, D, chi, chi)
-  temp = T4d.transpose(3, 0, 1, 2).reshape(chi, D**2*chi)
-  down = np.dot(down.reshape(D**2*chi, chi),temp).reshape(D, D, chi, chi, D, D)
-  down = down.transpose(1, 2, 3, 5, 0, 4).reshape(D**2*chi**2, D**2)
-  temp = Ad.transpose(3, 4, 0, 1, 2).reshape(D**2, D**2*d)
-  down = np.dot(down,temp).reshape(D, chi, chi, D, d, D, D)
-  down = down.transpose(1, 2, 4, 5, 6, 0, 3).reshape(D**2*chi**2*d, D**2)
-  temp = Ad.conj().transpose(3, 4, 0, 1, 2).reshape(D**2, D**2*d)
-  down = np.dot(down,temp).reshape(chi, chi, d, D, D, d, D, D)
-  down = down.transpose(1, 2, 3, 5, 6, 0, 4, 7).reshape(D**2*chi*d**2, D**2*chi)
-  temp = T2d.transpose(1, 2, 3, 0).reshape(D**2*chi, chi)
-  down = np.dot(down,temp).reshape(chi, d, D, d, D, chi)
-  up = up.transpose(1, 2, 3, 0).reshape(D**2*chi, chi)
-  up = np.dot(up,T2u.reshape(chi, D**2*chi)).reshape(D, D, chi, chi, D, D)
-  up = up.transpose(0, 2, 3, 4, 1, 5).reshape(D**2*chi**2, D**2)
-  temp = Au.conj().transpose(1, 2, 0, 3, 4).reshape(D**2, D**2*d)
-  up = np.dot(up,temp).reshape(D, chi, chi, D, d, D, D)
-  up = up.transpose(1, 2, 4, 5, 6, 0, 3).reshape(D**2*chi**2*d, D**2)
-  temp = Au.transpose(1, 2, 0, 3, 4).reshape(D**2, D**2*d)
-  up = np.dot(up,temp).reshape(chi, chi, d, D, D, d, D, D)
-  up = up.transpose(1, 2, 3, 5, 6, 0, 7, 4).reshape(D**2*chi*d**2, D**2*chi)
-  up = np.dot(up,T4u.reshape(D**2*chi, chi)).reshape(chi, d, D, d, D, chi)
-  up = up.transpose(1, 3, 5, 4, 2, 0).reshape(d**2, D**2*chi**2)
-  down = down.transpose(0, 2, 4, 5, 1, 3).reshape(D**2*chi**2, d**2)
+  down = np.dot(C4,T3.transpose(2, 0, 1).reshape(chi, l)).reshape(chi, D, D, chi)
+  down = np.dot(down.reshape(l, chi),C3.T).reshape(chi, l)
+  down = np.dot(down.T,T4d.transpose(2, 0, 1).reshape(chi, l)).reshape(D, D*chi**2, D, D)
+  down = down.transpose(1, 3, 0, 2).reshape(l*chi, D2)
+  down = np.dot(down,Ad).reshape(D, chi**2, D, d*D2)
+  down = down.transpose(1, 3, 0, 2).reshape(l*chi*d, D2)
+  down = np.dot(down,Ad.conj()).reshape(chi, chi*d*D, D, d*D, D)
+  down = down.transpose(1, 3, 0, 2, 4).reshape(l*d**2, l)
+  down = np.dot(down,T2d.transpose(1, 2, 0).reshape(l, chi)).reshape(chi, d, D, d, D*chi)
+  down = down.transpose(0, 2, 4, 1, 3).reshape(l*chi, d**2)
+  up = np.dot(C2,T1.reshape(chi, l))
+  up = np.dot(up.reshape(l, chi),C1).reshape(chi,l)
+  up = np.dot(up.T,T2u.reshape(chi, l)).reshape(D, D*chi**2, D, D)
+  up = up.transpose(1, 3, 0, 2).reshape(l*chi, D2)
+  up = np.dot(up,Au).reshape(D, chi**2, D, d*D2)
+  up = up.transpose(1, 3, 0, 2).reshape(l*chi*d, D2)
+  up = np.dot(up,Au.conj()).reshape(chi, chi*d*D, D, d*D, D)
+  up = up.transpose(1, 3, 0, 2, 4).reshape(l*d**2, l)
+  up = np.dot(up,T4u.reshape(l, chi)).reshape(chi, d, D, d, D, chi)
+  up = up.transpose(1, 3, 5, 2, 4, 0).reshape(d**2, l*chi)
   rdm = np.dot(up,down).reshape(d, d, d, d)
   del up,down
 
-  rdm = rdm.transpose(1, 2, 0, 3).reshape(d**2,d**2)
+  rdm = rdm.swapaxes(1,2).reshape(d**2,d**2)
   rdm = rdm/np.trace(rdm)
   return rdm
 
