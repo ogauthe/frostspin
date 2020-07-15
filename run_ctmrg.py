35253ee1540e07289bc2484fe68e7bdbec2c945c
Olivier Gauthé <olivier.gauthe@irsamc.ups-tlse.fr>
2020-07-16 00:11:05 +0200

    test for 3x3 unit cell

--- run_ctmrg.py
+++ run_ctmrg.py
@@ -22,6 +22,13 @@ BCA
 CAB
 """
 
+
+til9 = """
+ABC
+DEF
+GHI
+"""
+
 """
 tiling = tilingAB
 tensors = [tAKLT2.copy(),tAKLT2.copy()]
@@ -30,17 +37,48 @@ A = np.random.random((6,9,4,5,2,3))
 B = np.random.random((7,8,2,3,4,5))
 tensors = [A,B]
 tiling = tilingAB
-"""
 
 A = np.random.random((10,11,2,3,4,5))
 B = np.random.random((12,13,4,6,7,3))
 C = np.random.random((8,9,7,5,2,6))
 tensors = [A,B,C]
 tiling = tilingABC
+"""
 
+
+for i in range(1,19):
+  Ds = 2*np.ones(19,dtype=np.int8)
+  Ds[i] = 3
+  print('#'*79,f"\ni = {i}, Ds = {Ds}")
+
+  A = np.random.random((3,1,Ds[1],Ds[2],Ds[3],Ds[4]))
+  B = np.random.random((5,1,Ds[5],Ds[6],Ds[7],Ds[2]))
+  C = np.random.random((2,7,Ds[8],Ds[4],Ds[9],Ds[6]))
+  D = np.random.random((7,1,Ds[3],Ds[10],Ds[11],Ds[12]))
+  E = np.random.random((3,2,Ds[7],Ds[14],Ds[15],Ds[10]))
+  F = np.random.random((3,5,Ds[9],Ds[12],Ds[13],Ds[14]))
+  G = np.random.random((3,7,Ds[11],Ds[16],Ds[1],Ds[17]))
+  H = np.random.random((7,3,Ds[15],Ds[18],Ds[5],Ds[16]))
+  I = np.random.random((5,3,Ds[13],Ds[17],Ds[8],Ds[18]))
+
+
+  ctm = CTMRG([A,B,C,D,E,F,G,H,I],til9,chi,verbosity=0)
+  ctm.print_tensor_shapes()
+  for i in range(5):
+    ctm.iterate()
+  rdm1x1 = ctm.compute_rdm1x1()
+  rdm1x2 = ctm.compute_rdm1x2()
+  rdm2x1 = ctm.compute_rdm2x1()
+  rdm2x2 = ctm.compute_rdm2x2()
+
+  print(f'rdm1x1 is hermitian: {lg.norm(rdm1x1-rdm1x1.T.conj()):.1e}')
+  print(f'rdm1x2 is hermitian: {lg.norm(rdm1x2-rdm1x2.T.conj()):.1e}')
+  print(f'rdm2x1 is hermitian: {lg.norm(rdm2x1-rdm2x1.T.conj()):.1e}')
+  print(f'rdm2x2 is hermitian: {lg.norm(rdm2x2-rdm2x2.T.conj()):.1e}')
+
+"""
 ctm = CTMRG(tensors,tiling,chi,verbosity=2)
 ctm.print_tensor_shapes()
-
 print('try 2 iterations:')
 ctm.iterate()
 print("#"*40,'  1st done  ',"#"*40,sep="")
@@ -67,7 +105,7 @@ print(f'rdm1x2 is hermitian: {lg.norm(rdm1x2-rdm1x2.T.conj()):.1e}')
 print(f'rdm2x1 is hermitian: {lg.norm(rdm2x1-rdm2x1.T.conj()):.1e}')
 print(f'rdm2x2 is hermitian: {lg.norm(rdm2x2-rdm2x2.T.conj()):.1e}')
 
-"""
+
 print(f'trace(H_AKLT @ rdm1x2) = {np.trace(H_AKLT @ rdm1x2):.1e}')
 print(f'trace(H_AKLT @ rdm2x1) = {np.trace(H_AKLT @ rdm2x1):.1e}')
 
