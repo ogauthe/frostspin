762757ee860404fec31cc797cc6b2fc42d8d5a74
Olivier Gauthé <olivier.gauthe@irsamc.ups-tlse.fr>
2020-07-22 22:08:08 +0200

    run_SU to test code for J1=1,J2=0

--- run_SU.py
+++ run_SU.py
@@ -13,45 +13,89 @@ AB
 CD
 """
 
-def evalH(su,chi,niter=10):
-  A,B,C,D = su.get_ABCD()
-  ctm = CTMRG((A,B,C,D),tiling,chi,verbosity=2)
-  for i in range(niter):
-    ctm.iterate()
-
-  rdm1x2 = ctm.compute_rdm1x2()
-  return np.trace(su.h1 @ rdm1x2)
 
 d = 2
-a = 1
-chi = 11
-Ds = 2,3,4,5,6,7,9,10
-su_iter = 100
-print(f'run simple update with d = {d}, a = {a}, Ds = {Ds}, converge CTM with chi = {chi}')
+a = 2
+chi = 25
+niter = 10
+Ds = [5]*8
+print(f'run simple update with d = {d}, a = {a}, Ds = {Ds}')
 
-tau = 0.1
-su = SimpleUpdateABCD(d,a,Ds,SdS_22b, SdS_22b, tau)
+tau = 0.001
+su = SimpleUpdateABCD(d,a,Ds,SdS_22b, np.zeros((4,4)), tau)
 
-eps0 = evalH(su,chi,3)
-print(f'Random tensors: <h1> = {eps0:.4e}')
+print("Run simple update for spin 1/2 Heisenberg with J1=1, J2=0")
+print(f"run with tau = {tau} up to beta = 1")
+"""
+for i in range(np.rint(1/2/tau).astype(int)):
+  print(i, end=" ")
+  su.update_bond1()
+  su.update_bond2()
+  su.update_bond3()
+  su.update_bond4()
+  su.update_bond5()
+  su.update_bond6()
+  su.update_bond7()
+  su.update_bond8()
+  su.update_bond8()
+  su.update_bond7()
+  su.update_bond6()
+  su.update_bond5()
+  su.update_bond4()
+  su.update_bond3()
+  su.update_bond2()
+  su.update_bond1()
+"""
+print(f"done with SU. Converge CTMRG with chi = {chi} and niter = {niter}")
+ctm = CTMRG(su.get_ABCD(),tiling,chi,verbosity=0)
+for i in range(niter):
+  print(i)
+  ctm.iterate()
 
-print("test 1st neighbor updates")
-su.update_bond1()
-su.update_bond2()
-su.update_bond3()
-su.update_bond4()
-su.update_bond5()
-su.update_bond6()
-su.update_bond7()
-su.update_bond8()
-print("done")
+print("done with CTM. Compute rdm")
+rdmAB = ctm.compute_rdm1x2(0,0)
+rdmAC = ctm.compute_rdm2x1(0,0)
+rdmBA = ctm.compute_rdm1x2(1,0)
+rdmCA = ctm.compute_rdm2x1(0,1)
+eps = 0.5*np.trace(su.h1 @ (rdmAB + rdmAC + rdmBA + rdmCA))
+print(f"epsilon = {eps}")
 
 """
-print(f'Iter simple update for {su_iter} times', end='...')
-for i in range(su_iter):
-  su.update()
-print(' Done. Evaluate energy')
+su.update_bonds25() # through B
+su.update_bonds17() # through C
+su.update_bonds26() # through B
+su.update_bonds37() # through C
+su.update_bonds46() # through B
+su.update_bonds38() # through C
+su.update_bonds45() # through B
+su.update_bonds18() # through C
 
-eps1 = evalH(su,chi)
-print(f'Done, <h1> = {eps1:.4e}')
+su.update_bonds41() # through A
+su.update_bonds58() # through D
+su.update_bonds43() # through A
+su.update_bonds68() # through D
+su.update_bonds23() # through A
+su.update_bonds67() # through D
+su.update_bonds21() # through A
+su.update_bonds57() # through D
+
+su.update_bonds57() # through D
+su.update_bonds21() # through A
+su.update_bonds67() # through D
+su.update_bonds23() # through A
+su.update_bonds68() # through D
+su.update_bonds43() # through A
+su.update_bonds58() # through D
+su.update_bonds41() # through A
+
+su.update_bonds18() # through C
+su.update_bonds45() # through B
+su.update_bonds38() # through C
+su.update_bonds46() # through B
+su.update_bonds37() # through C
+su.update_bonds26() # through B
+su.update_bonds17() # through C
+su.update_bonds25() # through B
+print("done")
+
 """
