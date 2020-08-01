846cd1e2e915e3c86ab309ae87537d35b3a43798
Olivier Gauthé <olivier.gauthe@irsamc.ups-tlse.fr>
2020-08-01 04:49:50 +0200

    fix rotations on B site: put it in *operator* only

--- run_SU.py
+++ run_SU.py
@@ -6,6 +6,7 @@ from simple_updateAB import SimpleUpdateAB
 from simple_updateABCD import SimpleUpdateABCD
 from test_tools import SdS_22b, cconj2
 from CTMRG import CTMRG
+from toolsU1 import checkU1, combine_colors
 
 
 tiling = 'AB\nCD'
@@ -14,7 +15,7 @@ d = 2
 a = 2
 chi = 35
 ctm_iter = 100
-D = 5   # 2+2+1
+D = 5 # 3+1+1 at start
 Ds = (D,)*8
 print(f'run simple update with d = {d}, a = {a}, Ds = {Ds}')
 
@@ -23,7 +24,7 @@ tau = 0.001
 su_iter = int(beta/2/tau)  # rho is quadratic in psi
 
 # zero singular values lead to divide by zero when removing weights
-# to avoif this, start with tensors made of decoupled product of (2x2)->1 on
+# to avoid this, start with tensors made of decoupled product of (2x2)->1 on
 # physical x ancila and 3^4 -> 1 (B1) on virtual legs.
 p22_3333 = np.zeros((2,2,3,3,3,3),dtype=np.int8)
 p22_3333.flat[[89,95,103,105,115,119,123,127,137,139,147,153,170,176,184,186,
@@ -32,20 +33,71 @@ p22_3333.flat[[89,95,103,105,115,119,123,127,137,139,147,153,170,176,184,186,
 
 A = np.zeros((d,a,D,D,D,D))
 A[:,:,:3,:3,:3,:3] = p22_3333
-B = np.tensordot(cconj2,A,((1,),(0,)))   # rotation on B sites
+#B = np.tensordot(cconj2,A,((1,),(0,)))   # rotation on B sites
+B = A.copy()
 C = B.copy()
 D = A.copy()
 
-su = SimpleUpdateABCD(d,a,Ds,SdS_22b, np.zeros((4,4)), tau, tensors=(A,B,C,D))
-#su = SimpleUpdateABCD(d,1,Ds,SdS_22b, np.zeros((4,4)), tau)
+# colors A and D = [[1,-1], [1,-1],[2, 0, -2, 0, 0],[2, 0, -2, 0, 0],[2, 0, -2, 0, 0],[2, 0, -2, 0, 0]]
+# opposed colors on B and C physical leg
+# colors B and C = [[-1,1], [1,-1],[2, 0, -2, 0, 0],[2, 0, -2, 0, 0],[2, 0, -2, 0, 0],[2, 0, -2, 0, 0]]
 
+#su = SimpleUpdateABCD(d,a,Ds,SdS_22b, np.zeros((4,4)), tau, tensors=(A,B,C,D))
+#su = SimpleUpdateABCD(d,1,Ds,SdS_22b, np.zeros((4,4)), tau, verbosity=10)
+pcol = np.array([1,-1],dtype=np.int8)
+vcol = np.array([2,0,-2,0,0],dtype=np.int8)
+colors = [pcol,pcol,vcol,vcol,vcol,vcol,vcol,vcol,vcol,vcol]
+#su = SimpleUpdateABCD(d,a,Ds,SdS_22b, np.zeros((4,4)), tau, tensors=(A,B,C,D), verbosity=10)
+print(checkU1(A,[pcol,pcol,vcol,vcol,vcol,vcol]))
+print(checkU1(B,[-pcol,-pcol,-vcol,-vcol,-vcol,-vcol]))
+print(checkU1(SdS_22b,[combine_colors(pcol,-pcol),-combine_colors(pcol,-pcol)]))
+#print(checkU1(B,[pcol,-pcol,-vcol,-vcol,-vcol,-vcol]))
+su = SimpleUpdateABCD(d,a,Ds,SdS_22b, np.zeros((4,4)), tau, tensors=(A,B,C,D), colors=colors, verbosity=10)
 
+print("#"*79)
+su.update_bond1()
+su.update_bond2()
+su.update_bond3()
+su.update_bond4()
+su.update_bond5()
+su.update_bond6()
+su.update_bond7()
+su.update_bond8()
+print("#"*79)
+su.update_bond8()
+su.update_bond7()
+su.update_bond6()
+su.update_bond5()
+su.update_bond4()
+su.update_bond3()
+su.update_bond2()
+su.update_bond1()
+print("#"*79)
+su.update_bond1()
+su.update_bond2()
+su.update_bond3()
+su.update_bond4()
+su.update_bond5()
+su.update_bond6()
+su.update_bond7()
+su.update_bond8()
+print("#"*79)
+su.update_bond8()
+su.update_bond7()
+su.update_bond6()
+su.update_bond5()
+su.update_bond4()
+su.update_bond3()
+su.update_bond2()
+su.update_bond1()
+
+"""
 print("Run simple update for spin 1/2 Heisenberg with J1=1, J2=0")
 print(f"run with tau = {tau} up to beta = {beta}")
 
 #for i in range(su_iter//2): # 2nd order Trotter
-for i in range(su_iter): # 1st order
-  print(i, end=" ")
+for i in range(5): # 1st order
+  print("iter", i)
   su.update_bond1()
   su.update_bond2()
   su.update_bond3()
@@ -54,7 +106,7 @@ for i in range(su_iter): # 1st order
   su.update_bond6()
   su.update_bond7()
   su.update_bond8()
-"""
+
   su.update_bond8()
   su.update_bond7()
   su.update_bond6()
@@ -63,7 +115,7 @@ for i in range(su_iter): # 1st order
   su.update_bond3()
   su.update_bond2()
   su.update_bond1()
-"""
+
 print(f"\ndone with SU. Converge CTMRG with chi = {chi} and niter = {ctm_iter}")
 ctm = CTMRG(su.get_ABCD(),tiling,chi,verbosity=0)
 rdmD = ctm.compute_rdm1x1(0,0)
@@ -101,7 +153,7 @@ print("\ndone with CTM. Compute rdm")
 eps = 0.5*np.trace(su.h1 @ (rdmAB + rdmAC + rdmBA + rdmCA))
 print(f"epsilon = {eps}")
 
-"""
+
 su.update_bonds25() # through B
 su.update_bonds17() # through C
 su.update_bonds26() # through B
