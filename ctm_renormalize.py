fix color sign

ctm_renormalize.py
@@ -6,7 +6,7 @@ from toolsU1 import default_color
 def construct_projectors(R,Rt,chi,color=default_color):
   # row and column colors are the same since contraction can be done on the
   # 2 other legs of R and Rt
-  U,s,V,col = svdU1_truncate(R.T @ Rt, chi, color, -color)
+  U,s,V,col = svdU1_truncate(R.T @ Rt, chi, color, color)
   s12 = 1/np.sqrt(s)  # s contains no 0
   # convention: projectors have shape (last_chi*D**2,chi)
   # since values of last_chi and D are not known (nor used) here