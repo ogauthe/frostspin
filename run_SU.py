e969da2f16cae95e406d707b679e22d4ff6a2ef5
Olivier Gauthé <olivier.gauthe@epfl.ch>
2020-09-29 18:57:52 +0200

    working code for several betas and several chis

--- run_SU.py
+++ run_SU.py
@@ -8,36 +8,6 @@ from simple_update import SimpleUpdate2x2
 from ctmrg import CTMRG
 
 
-def compute_energy(ctm, h1, h2):
-    """
-    Compute mean per site site energy on a 2x2 plaquette with first neighbor Hamiltonian
-    h1 and second neighbor Hamiltonian h2 from CTMRG.
-    """
-    # Tr(AH) = (A*H.T).sum() and H is exactly symmetric
-    rho1 = ctm.compute_rdm2x1(-1, 0)
-    rho2 = ctm.compute_rdm1x2(-1, -1)
-    rho3 = ctm.compute_rdm2x1(-1, -1)
-    rho4 = ctm.compute_rdm1x2(0, -1)
-    rho5 = ctm.compute_rdm2x1(0, 0)
-    rho6 = ctm.compute_rdm2x1(0, -1)
-    rho7 = ctm.compute_rdm1x2(-1, 0)
-    rho8 = ctm.compute_rdm1x2(0, 0)
-    eps1 = ((rho1 + rho2 + rho3 + rho4 + rho5 + rho6 + rho7 + rho8) * h1).sum()
-
-    rho9 = ctm.compute_rdm_diag_dr(0, 0)
-    rho10 = ctm.compute_rdm_diag_ur(-1, 0)
-    rho11 = ctm.compute_rdm_diag_dr(-1, -1)
-    rho12 = ctm.compute_rdm_diag_ur(0, -1)
-    rho13 = ctm.compute_rdm_diag_dr(-1, 0)
-    rho14 = ctm.compute_rdm_diag_ur(0, 0)
-    rho15 = ctm.compute_rdm_diag_dr(0, -1)
-    rho16 = ctm.compute_rdm_diag_ur(-1, -1)
-    eps2 = ((rho9 + rho10 + rho11 + rho12 + rho13 + rho14 + rho15 + rho16) * h2).sum()
-
-    energy = (eps1 + eps2) / 4
-    return energy
-
-
 ########################################################################################
 # Initialization
 ########################################################################################
@@ -79,75 +49,98 @@ h2 = J2 * SdS_22
 print(f"Physical model: d = {d}, a = {a}, J1 = 1, J2 = {J2}")
 
 # simple update parameters
-beta = config["beta"]
 tau = config["tau"]
 Dmax = config["Dmax"]
-print(f"Simple update parameters: beta = {beta}, tau = {tau}, Dmax = {Dmax}")
+beta_list = np.array(config["beta_list"], dtype=float)
+print(f"Simple update parameters: tau = {tau}, Dmax = {Dmax}")
+print("Compute environment and observables for beta in", list(beta_list))
+pcol = np.array([1, -1], dtype=np.int8)  # U(1) colors for physical spin 1/2
+su = SimpleUpdate2x2(d, a, Dmax, h1, h2, tau, colors=pcol, verbosity=0)
 
 # CTMRG parameters
-chi = config["chi"]
+ctm_list = []
 ctm_iter = config["ctm_iter"]
-print(f"CTMRG parameters: chi = {chi}, ctm_iter = {ctm_iter}")
+chi_list = np.array(config["chi_list"], dtype=int)
+print(f"Converge CTMRG for chi in {list(chi_list)} for ctm_iter = {ctm_iter}")
 
 # misc
-print("Save data in file", config["save_data"])
-pcol = np.array([1, -1], dtype=np.int8)  # U(1) colors for physical spin 1/2
-
-
-########################################################################################
-# Simple update
-########################################################################################
-print("\n" + "#" * 79)
-print(f"Start simple update with tau = {tau} up to beta/2 = {beta/2}")
-su = SimpleUpdate2x2(d, a, Dmax, h1, h2, tau, colors=pcol, verbosity=1)
-
-t = time()
-su.evolve(beta / 2)  # rho is quadratic in mono-layer tensor
-print(f"\nDone with SU, t = {time()-t:.0f}")
-
-lambdas = su.lambdas
-(pcol, acol), col1, col2, col3, col4, col5, col6, col7, col8 = su.colors
+print("Save simple update data in files " + config["save_su_root"] + "{beta}.npz")
+print("Save CTMRG data in files " + config["save_ctm_root"] + "{beta}_chi{chi}.npz")
 print(
-    "lambdas =",
-    lambdas[1],
-    lambdas[2],
-    lambdas[3],
-    lambdas[4],
-    lambdas[5],
-    lambdas[6],
-    lambdas[7],
-    lambdas[8],
-    sep="\n",
-)
-print("colors =", col1, col2, col3, col4, col5, col6, col7, col8, sep="\n")
-
-data_su = su.save_to_file()
-np.savez_compressed(config["save_data"], **data_su)
-print("Simple update data saved in file", config["save_data"])
-
-########################################################################################
-# CTMRG
-########################################################################################
-print("\n" + "#" * 79)
-print("Compute observables using CTMRG")
-ctm = CTMRG(
-    chi, tensors=su.get_ABCD(), tiling=tiling, colors=su.get_colors_ABCD(), verbosity=0
+    "Save reduced density matrices in files "
+    + config["save_rdm_root"]
+    + "{beta}_chi{chi}.npz"
 )
 
-print(f"energy before iteration = {compute_energy(ctm, h1, h2)}")
-print(f"Converge CTMRG for {ctm_iter} iterations with D = {Dmax} and chi = {chi}")
+
+########################################################################################
+# Computation starts
+########################################################################################
 t = time()
-for i in range(ctm_iter):
-    ctm.iterate()
-    energy = compute_energy(ctm, h1, h2)
-    print(f"i = {i+1}, t = {time()-t:.0f}, energy = {energy}")
+beta = 0.0
+for new_beta in beta_list:
+    ####################################################################################
+    # Simple update
+    ####################################################################################
+    print("\n" + "#" * 79)
+    print(f"Evolve in imaginary time for beta from {beta}/2 to {new_beta}/2...")
+    su.evolve((new_beta - beta) / 2)  # rho is quadratic in mono-layer tensor
+    print(f"done with imaginary time evolution, t = {time()-t:.0f}")
+    lambdas = su.lambdas
+    _, col1, col2, col3, col4, col5, col6, col7, col8 = su.colors
+    print(
+        "lambdas =",
+        lambdas[1],
+        lambdas[2],
+        lambdas[3],
+        lambdas[4],
+        lambdas[5],
+        lambdas[6],
+        lambdas[7],
+        lambdas[8],
+        sep="\n",
+    )
+    print("colors =", col1, col2, col3, col4, col5, col6, col7, col8, sep="\n")
+    save_su = config["save_su_root"] + f"{beta}.npz"
+    su.save_to_file(save_su)
+    print("Simple update data saved in file", save_su)
+    tensors = su.get_ABCD()
+    colors = su.get_colors_ABCD()
 
-print(f"\ndone with CTM iteration, t={time()-t:.0f}")
-print("energy =", energy)
+    if not beta:  # initialize CTMRG from 1st beta value
+        for chi in chi_list:
+            ctm_list.append(CTMRG(chi, tiling=tiling, tensors=tensors, colors=colors))
+    else:  # set tensors to new values
+        for ctm in ctm_list:
+            ctm.set_tensors(tensors, colors)
+    beta = new_beta
 
-data_ctm = ctm.save_to_file()
-np.savez_compressed(config["save_data"], **data_su, **data_ctm)
-print("Simple update and CTMRG data saved in file", config["save_data"])
+    ####################################################################################
+    # CTMRG
+    ####################################################################################
+    for ctm in ctm_list:
+        print("\n" + " " * 4 + "#" * 75)
+        print(f"    Converge CTMRG for D = {Dmax} and chi = {ctm.chi}...")
+        for i in range(10):  # CONVERGENCE CHECKS ARE NEEDED
+            ctm.iterate()
+        print(f"    done with CTM iterations, t={time()-t:.0f}")
+        save_ctm = config["save_ctm_root"] + f"{beta}_chi{ctm.chi}.npz"
+        ctm.save_to_file(save_ctm)
+        print("    CTMRG data saved in file", save_ctm)
+
+        print(
+            "    Compute reduced density matrix cell average for first and",
+            "second neighbors...",
+        )
+        rdm_1st_nei = ctm.compute_rdm_cell_average_1st_nei()
+        rdm_2nd_nei = ctm.compute_rdm_cell_average_2nd_nei()
+        print(f"    done with rdm computation, t={time()-t:.0f}")
+        save_rdm = config["save_rdm_root"] + f"{beta}_chi{ctm.chi}.npz"
+        np.savez_compressed(save_rdm, rdm_1st_nei=rdm_1st_nei, rdm_2nd_nei=rdm_2nd_nei)
+        print("    rdm saved in file", save_rdm)
+        energy = (rdm_1st_nei * h1).sum() + (rdm_2nd_nei * h2).sum()
+        print(f"    energy = {energy}")
+
 
 print("\n" + "#" * 79)
-print("done")
+print("Program terminated.")
