d03d45ca4ba6160c3af510e3b7527b798316d920
Olivier Gauthé <olivier.gauthe@epfl.ch>
2020-10-22 20:31:56 +0200

    add compute_rdm_2nd_nei arg

--- run_CTMRG.py
+++ run_CTMRG.py
@@ -89,9 +89,30 @@ ctm_params = {
     "ctm_maxiter": ctm_maxiter,
 }
 save_ctm_root = str(config["save_ctm_root"])
-save_rdm_root = str(config["save_rdm_root"])
 print("Save CTMRG data in files " + save_ctm_root + "{beta}_chi{chi}.npz")
-print("Save reduced density matrices in files " + save_rdm_root + "{beta}_chi{chi}.npz")
+
+# observables
+compute_rdm_2nd_nei = bool(config["compute_rdm_2nd_nei"])
+if compute_rdm_2nd_nei:
+    print(
+        "Compute next nearest neighbor density matrix and observables once",
+        "environment is converged",
+    )
+    print("Compute capacity as -(beta+dbeta/2)**2 * (E(beta) - E(beta+dbeta)) / dbeta")
+    save_rdm_root = str(config["save_rdm_root"])
+    print(
+        "Save reduced density matrices in files "
+        + save_rdm_root
+        + "{beta}_chi{chi}.npz"
+    )
+    energies0 = []
+    order_parameter0 = []
+    energies1 = []
+    order_parameter1 = []
+    capacities = []
+else:
+    print("Do not compute next nearest neighbor density matrix")
+
 
 # CTMRG initialization
 print()
@@ -102,55 +123,9 @@ if config["ctm_restart_file"] is not None:
 else:
     ctm = CTMRG_U1(chi, tiling, su.get_ABCD(), su.get_colors_ABCD(), verbosity=1)
 
-# misc
-rdm_params = {"cell_coords": ctm.neq_coords.copy(), **ctm_params}  # need ctm
-energies0 = []
-order_parameter0 = []
-energies1 = []
-order_parameter1 = []
 
-
-# actual computation steps
-def converge_compute_rdm(ctm, beta):
-    print(f"Converge CTMRG for D = {Dmax} and chi = {ctm.chi}...")
-    t = time.time()
-    i, (rdm2x1_cell, rdm1x2_cell) = ctm.converge(
-        ctm_tol, warmup=ctm_warmup, maxiter=ctm_maxiter
-    )
-    print(f"done, converged after {i} iterations, t = {time.time()-t:.0f}")
-    save_ctm = save_ctm_root + f"{beta}_chi{ctm.chi}.npz"
-    data_ctm = ctm.save_to_file()
-    np.savez_compressed(save_ctm, beta=beta, chi=ctm.chi, **ctm_params, **data_ctm)
-    print("CTMRG data saved in file", save_ctm)
-
-    print("Compute reduced density matrix for all second neighbor bonds...")
-    rdm2x1_cell = np.array(rdm2x1_cell)
-    rdm1x2_cell = np.array(rdm1x2_cell)
-    save_rdm = save_rdm_root + f"{beta}_chi{ctm.chi}.npz"
-    t = time.time()
-    rdm_dr_cell, rdm_ur_cell = ctm.compute_rdm_2nd_neighbor_cell()
-    rdm_dr_cell = np.array(rdm_dr_cell)
-    rdm_ur_cell = np.array(rdm_ur_cell)
-    print(f"done with rdm computation, t = {time.time()-t:.0f}")
-    np.savez_compressed(
-        save_rdm,
-        beta=beta,
-        chi=ctm.chi,
-        rdm1x2_cell=rdm1x2_cell,
-        rdm2x1_cell=rdm2x1_cell,
-        rdm_dr_cell=rdm_dr_cell,
-        rdm_ur_cell=rdm_ur_cell,
-        **rdm_params,
-    )
-    print("rdm saved to file", save_rdm)
-    energy = ((rdm1x2_cell + rdm2x1_cell) * h1).sum() / 4 + (
-        (rdm_dr_cell + rdm_ur_cell) * h2
-    ).sum() / 4
-    order_parameter = ((rdm1x2_cell - rdm2x1_cell) * h1).sum()
-    print(f"energy = {energy}")
-    print(f"Ising order parameter = {order_parameter}")
-    return energy, order_parameter
-
+if compute_rdm_2nd_nei:
+    rdm_params = {"cell_coords": ctm.neq_coords.copy(), **ctm_params}  # need ctm
 
 ########################################################################################
 # Computation for beta = beta0
@@ -161,14 +136,51 @@ print("colors =", *su.colors[1:], sep="\n")
 for chi in chi_list:
     print("", "#" * 75, f"Set chi to {chi}", sep="\n")
     ctm.chi = chi  # restart from last envrionment, just change chi
-    energy, op = converge_compute_rdm(ctm, beta0)
-    energies0.append(energy)
-    order_parameter0.append(op)
+    print(f"Converge CTMRG for D = {Dmax} and chi = {ctm.chi}...")
+    t = time.time()
+    i, (rdm2x1_cell, rdm1x2_cell) = ctm.converge(
+        ctm_tol, warmup=ctm_warmup, maxiter=ctm_maxiter
+    )
+    print(f"done, converged after {i} iterations, t = {time.time()-t:.0f}")
+    save_ctm = save_ctm_root + f"{beta0}_chi{ctm.chi}.npz"
+    data_ctm = ctm.save_to_file()
+    np.savez_compressed(save_ctm, beta=beta0, chi=ctm.chi, **ctm_params, **data_ctm)
+    del data_ctm
+    print("CTMRG data saved in file", save_ctm)
 
-print("\n" + "#" * 79)
-print(f"Energy and order parameter for D = {Dmax}, tau = {tau}, beta = {beta0}:")
-for chi, eps, op in zip(chi_list, energies0, order_parameter0):
-    print(f"chi = {chi}: energy = {eps:.17f}, order_parameter = {op:.17f}")
+    if compute_rdm_2nd_nei:
+        rdm2x1_cell = np.array(rdm2x1_cell)
+        rdm1x2_cell = np.array(rdm1x2_cell)
+        print("\nCompute reduced density matrix for all second neighbor bonds...")
+        t = time.time()
+        rdm_dr_cell, rdm_ur_cell = np.array(ctm.compute_rdm_2nd_neighbor_cell())
+        print(f"done with rdm computation, t = {time.time()-t:.0f}")
+        save_rdm = save_rdm_root + f"{beta0}_chi{ctm.chi}.npz"
+        np.savez_compressed(
+            save_rdm,
+            beta=beta0,
+            chi=ctm.chi,
+            rdm1x2_cell=rdm1x2_cell,
+            rdm2x1_cell=rdm2x1_cell,
+            rdm_dr_cell=rdm_dr_cell,
+            rdm_ur_cell=rdm_ur_cell,
+            **rdm_params,
+        )
+        print("rdm saved to file", save_rdm)
+        energy = ((rdm1x2_cell + rdm2x1_cell) * h1).sum() / 4 + (
+            (rdm_dr_cell + rdm_ur_cell) * h2
+        ).sum() / 4
+        op = ((rdm1x2_cell - rdm2x1_cell) * h1).sum()
+        print(f"energy = {energy}")
+        print(f"Ising order parameter = {op}")
+        energies0.append(energy)
+        order_parameter0.append(op)
+
+print("", "#" * 79, f"Done for beta = {beta0}", sep="\n")
+if compute_rdm_2nd_nei:
+    print(f"Energy and order parameter for D = {Dmax}, tau = {tau}, beta = {beta0}:")
+    for chi, eps, op in zip(chi_list, energies0, order_parameter0):
+        print(f"chi = {chi}: energy = {eps:.17f}, order_parameter = {op:.6f}")
 
 ########################################################################################
 # Imaginary time evolution for minimal step
@@ -187,26 +199,64 @@ print("colors =", *su.colors[1:], sep="\n")
 # Computation for beta = beta1
 ########################################################################################
 # restart CTMRG from environment with same chi at previous temperature value beta0
-for chi in chi_list:
+for i, chi in enumerate(chi_list):
     print("\n" + "#" * 75)
-    save_ctm = save_ctm_root + f"{beta0}_chi{ctm.chi}.npz"
+    save_ctm = save_ctm_root + f"{beta1}_chi{ctm.chi}.npz"
     print("restart CTMRG from file", save_ctm)
     ctm = CTMRG_U1(chi, tiling, file=save_ctm, verbosity=1)
-    energy, op = converge_compute_rdm(ctm, beta1)
-    energies1.append(energy)
-    order_parameter1.append(op)
+    print(f"Converge CTMRG for D = {Dmax} and chi = {ctm.chi}...")
+    t = time.time()
+    j, (rdm2x1_cell, rdm1x2_cell) = ctm.converge(
+        ctm_tol, warmup=ctm_warmup, maxiter=ctm_maxiter
+    )
+    print(f"done, converged after {j} iterations, t = {time.time()-t:.0f}")
+    save_ctm = save_ctm_root + f"{beta1}_chi{ctm.chi}.npz"
+    data_ctm = ctm.save_to_file()
+    np.savez_compressed(save_ctm, beta=beta1, chi=ctm.chi, **ctm_params, **data_ctm)
+    del data_ctm
+    print("CTMRG data saved in file", save_ctm)
+
+    if compute_rdm_2nd_nei:
+        rdm2x1_cell = np.array(rdm2x1_cell)
+        rdm1x2_cell = np.array(rdm1x2_cell)
+        print("Compute reduced density matrix for all second neighbor bonds...")
+        t = time.time()
+        rdm_dr_cell, rdm_ur_cell = np.array(ctm.compute_rdm_2nd_neighbor_cell())
+        print(f"done with rdm computation, t = {time.time()-t:.0f}")
+        save_rdm = save_rdm_root + f"{beta1}_chi{ctm.chi}.npz"
+        np.savez_compressed(
+            save_rdm,
+            beta=beta1,
+            chi=ctm.chi,
+            rdm1x2_cell=rdm1x2_cell,
+            rdm2x1_cell=rdm2x1_cell,
+            rdm_dr_cell=rdm_dr_cell,
+            rdm_ur_cell=rdm_ur_cell,
+            **rdm_params,
+        )
+        print("rdm saved to file", save_rdm)
+        energy = ((rdm1x2_cell + rdm2x1_cell) * h1).sum() / 4 + (
+            (rdm_dr_cell + rdm_ur_cell) * h2
+        ).sum() / 4
+        op = ((rdm1x2_cell - rdm2x1_cell) * h1).sum()
+        energies1.append(energy)
+        order_parameter1.append(op)
+        cap = -((beta0 + beta1) / 2) ** 2 * (energy - energies0[i]) / (beta1 - beta0)
+        capacities.append(cap)
+        print(f"energy = {energy}")
+        print(f"Ising order parameter = {op}")
+        print(f"thermal capacity = {cap}")
 
 
 print("\n" + "#" * 79)
 print("Computations finished.")
-print(f"Energy and order parameter for D = {Dmax}, tau = {tau}, beta = {beta1}:")
-for chi, eps, op in zip(chi_list, energies1, order_parameter1):
-    print(f"chi = {chi}: energy = {eps:.17f}, order_parameter = {op:.17f}")
-
-energies0 = np.array(energies0)
-energies1 = np.array(energies1)
-print("\nCompute capacity as c = -beta**2 * (E(beta1) - E(beta0)) / (beta1 - beta0)")
-print("with beta = (beta1 + beta0)/2\n")
-capacities = -(((beta0 + beta1) / 2) ** 2) * (energies1 - energies0) / (beta1 - beta0)
-for chi, cap in zip(chi_list, capacities):
-    print(f"chi = {chi}: capacity =  {cap}")
+if compute_rdm_2nd_nei:
+    print(
+        f"Energy and order parameter and capacity for D = {Dmax}, tau = {tau},",
+        f"beta = {beta1}:",
+    )
+    for chi, eps, op, cap in zip(chi_list, energies1, order_parameter1, capacities):
+        print(
+            f"chi = {chi}: energy = {eps:.17f}, order_parameter = {op:.6f},",
+            f"capacity = {cap:.6f}",
+        )
