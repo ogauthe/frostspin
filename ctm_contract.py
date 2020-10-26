6700acc42689eea666b5faaaf0cdba05857756f5
Olivier Gauthé <olivier.gauthe@epfl.ch>
2020-10-26 22:56:30 +0100

    use generic constact_corner function. Write explicit copies.

--- ctm_contract.py
+++ ctm_contract.py
@@ -9,7 +9,10 @@ Library agnostic module, only calls __matmul__, reshape and transpose methods.
 ###############################################################################
 
 
-def contract_ul_corner(C1, T1, T4, A):
+def contract_corner(C1, T1, T4, A):
+    """
+    Contract a corner C-T//T-A. Take upper left corner as template.
+    """
     #  C1-03-T1-2
     #  |     ||
     #  1->3  01
@@ -27,8 +30,13 @@ def contract_ul_corner(C1, T1, T4, A):
     ul = (ul @ T4.reshape(T4.shape[0], T4.shape[1] ** 2 * T4.shape[3])).reshape(
         T1.shape[1], T1.shape[2], T1.shape[0], T4.shape[1], T4.shape[2], T4.shape[3]
     )
-    ul = ul.transpose(0, 3, 1, 4, 2, 5).reshape(
-        T1.shape[1] * T4.shape[1], T1.shape[2] * T4.shape[2] * T1.shape[0] * T4.shape[3]
+    ul = (
+        ul.transpose(0, 3, 1, 4, 2, 5)
+        .copy()
+        .reshape(
+            T1.shape[1] * T4.shape[1],
+            T1.shape[2] * T4.shape[2] * T1.shape[0] * T4.shape[3],
+        )
     )
     temp = A.transpose(0, 1, 3, 4, 2, 5).reshape(
         A.shape[0] * A.shape[1] * A.shape[3] * A.shape[4], A.shape[2] * A.shape[5]
@@ -48,9 +56,13 @@ def contract_ul_corner(C1, T1, T4, A):
         T1.shape[2] * T4.shape[2],
         T1.shape[0] * T4.shape[3],
     )
-    ul = ul.swapaxes(1, 2).reshape(
-        A.shape[0] * A.shape[1] * T1.shape[2] * T4.shape[2],
-        A.shape[3] * A.shape[4] * T1.shape[0] * T4.shape[3],
+    ul = (
+        ul.swapaxes(1, 2)
+        .copy()
+        .reshape(
+            A.shape[0] * A.shape[1] * T1.shape[2] * T4.shape[2],
+            A.shape[3] * A.shape[4] * T1.shape[0] * T4.shape[3],
+        )
     )  # memory peak 2*a*d*chi**2*D**4
     #  C1----T1-6
     #  |     ||
@@ -66,6 +78,7 @@ def contract_ul_corner(C1, T1, T4, A):
         )
         .swapaxes(0, 1)
         .conj()
+        .copy()
         .reshape(
             A.shape[3] * A.shape[4], A.shape[0] * A.shape[1] * A.shape[2] * A.shape[5]
         )
@@ -80,28 +93,32 @@ def contract_ul_corner(C1, T1, T4, A):
     #  |  ||
     #  5  31
     #  3  45
-    return ul.transpose(4, 2, 0, 5, 3, 1)  # do not reshape to avoid copy here
+    return ul  # do not reshape to avoid copy here
+
+
+def contract_ul_corner(C1, T1, T4, A):
+    return contract_corner(C1, T1, T4, A).transpose(4, 2, 0, 5, 3, 1)
 
 
 def contract_ur_corner(T1, C2, A, T2):
-    return contract_ul_corner(
+    return contract_corner(
         C2, T2.transpose(1, 2, 3, 0), T1, A.transpose(0, 1, 3, 4, 5, 2)
-    )
+    ).transpose(4, 2, 0, 5, 3, 1)
 
 
 def contract_dr_corner(A, T2, T3, C3):
-    return contract_ul_corner(
+    return contract_corner(
         C3.T,
         T3.transpose(3, 0, 1, 2),
         T2.transpose(1, 2, 3, 0),
         A.transpose(0, 1, 4, 5, 2, 3),
-    ).transpose(3, 4, 5, 0, 1, 2)
+    ).transpose(5, 3, 1, 4, 2, 0)
 
 
 def contract_dl_corner(T4, A, C4, T3):
-    return contract_ul_corner(
+    return contract_corner(
         C4, T4, T3.transpose(3, 0, 1, 2), A.transpose(0, 1, 5, 2, 3, 4)
-    )
+    ).transpose(4, 2, 0, 5, 3, 1)
 
 
 ###############################################################################
@@ -111,9 +128,9 @@ def contract_dl_corner(T4, A, C4, T3):
 
 
 def contract_u_half(C1, T1l, T1r, C2, T4, Al, Ar, T2):
-    ul = contract_ul_corner(C1, T1l, T4, Al)
+    ul = contract_ul_corner(C1, T1l, T4, Al).copy()
     ul = ul.reshape(T1l.shape[0] * Al.shape[3] ** 2, T4.shape[3] * Al.shape[4] ** 2)
-    ur = contract_ur_corner(T1r, C2, Ar, T2)
+    ur = contract_ur_corner(T1r, C2, Ar, T2).copy()
     ur = ur.reshape(T2.shape[1] * Ar.shape[4] ** 2, T1r.shape[3] * Ar.shape[5] ** 2)
     #  UL-01-UR
     #  |      |
@@ -122,9 +139,9 @@ def contract_u_half(C1, T1l, T1r, C2, T4, Al, Ar, T2):
 
 
 def contract_l_half(C1, T1, T4u, Au, T4d, Ad, C4, T3):
-    ul = contract_ul_corner(C1, T1, T4u, Au)
+    ul = contract_ul_corner(C1, T1, T4u, Au).copy()
     ul = ul.reshape(T1.shape[0] * Au.shape[3] ** 2, T4u.shape[3] * Au.shape[4] ** 2)
-    dl = contract_dl_corner(T4d, Ad, C4, T3)
+    dl = contract_dl_corner(T4d, Ad, C4, T3).copy()
     dl = dl.reshape(T4d.shape[0] * Ad.shape[2] ** 2, T3.shape[2] * Ad.shape[3] ** 2)
     #  UL-0
     #  |
@@ -136,9 +153,9 @@ def contract_l_half(C1, T1, T4u, Au, T4d, Ad, C4, T3):
 
 
 def contract_d_half(T4, Al, Ar, T2, C4, T3l, T3r, C3):
-    dl = contract_dl_corner(T4, Al, C4, T3l)
+    dl = contract_dl_corner(T4, Al, C4, T3l).copy()
     dl = dl.reshape(T4.shape[0] * Al.shape[2] ** 2, T3l.shape[2] * Al.shape[3] ** 2)
-    dr = contract_dr_corner(Ar, T2, T3r, C3).transpose(3, 4, 5, 0, 1, 2)
+    dr = contract_dr_corner(Ar, T2, T3r, C3).transpose(3, 4, 5, 0, 1, 2).copy()
     dr = dr.reshape(T3r.shape[3] * Ar.shape[5] ** 2, T2.shape[0] * Ar.shape[2] ** 2)
     #  0      1
     #  0      0
@@ -148,9 +165,9 @@ def contract_d_half(T4, Al, Ar, T2, C4, T3l, T3r, C3):
 
 
 def contract_r_half(T1, C2, Au, T2u, Ad, T2d, T3, C3):
-    ur = contract_ur_corner(T1, C2, Au, T2u)
+    ur = contract_ur_corner(T1, C2, Au, T2u).copy()
     ur = ur.reshape(T2u.shape[1] * Au.shape[4] ** 2, T1.shape[3] * Au.shape[5] ** 2)
-    dr = contract_dr_corner(Ad, T2d, T3, C3).transpose(3, 4, 5, 0, 1, 2)
+    dr = contract_dr_corner(Ad, T2d, T3, C3).transpose(3, 4, 5, 0, 1, 2).copy()
     dr = dr.reshape(T3.shape[3] * Ad.shape[5] ** 2, T2d.shape[0] * Ad.shape[2] ** 2)
     #      1-UR
     #         |
