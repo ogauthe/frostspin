25439190715dfa237f2f226e4b08ede6f497327a
Olivier Gauthé <olivier.gauthe@epfl.ch>
2021-03-17 15:29:53 +0100

    use searchsorted with sorter=so1 instead of sorted_transposed_nnz1

--- groups/su2_matrix.py
+++ groups/su2_matrix.py
@@ -234,17 +234,16 @@ def construct_transpose_matrix(
 
     # 1) reformulate nnz1 in terms of proj2 leg ordering
     sh2 = tuple(r.dim for r in rep_bra2) + tuple(r.dim for r in rep_ket2)
-    cumprod1 = np.array((1,) + sh1[:0:-1]).cumprod()[::-1]
-    cumprod2 = np.array((1,) + sh2[:0:-1]).cumprod()[::-1]
-    transposed_nnz1 = (nnz1[:, None] // cumprod1 % sh1)[:, swap] @ cumprod2
+    strides1 = np.array((1,) + sh1[:0:-1]).cumprod()[::-1]
+    strides2 = np.array((1,) + sh2[:0:-1]).cumprod()[::-1]
+    transposed_nnz1 = (nnz1[:, None] // strides1 % sh1)[:, swap] @ strides2
 
     # 2) transposed_nnz1 and nnz2 contain the same values in different order. We want
     # the permutation linking them. Better to sort first then use binary search.
-    so1 = np.argsort(transposed_nnz1)
-    sorted_transposed_nnz1 = transposed_nnz1[so1]
-    perm = np.searchsorted(sorted_transposed_nnz1, nnz2)
+    so1 = transposed_nnz1.argsort()
+    perm = np.searchsorted(transposed_nnz1, nnz2, sorter=so1)
 
-    # 3) perm sends nnz2 to sorted_transposed_nnz1. We can first swap proj2 according
+    # 3) perm sends nnz2 to sorted(transposed_nnz1). We can first swap proj2 according
     # to perm, then re-swap it under argsort(so1) - or in one row swap proj2 according
     # to perm[argsort[so1] - to get transposed_nnz1. Avoid 2nd sort by swapping perm1
     proj1 = proj1[so1[perm]]
