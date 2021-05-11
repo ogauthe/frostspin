a1fcf40ed1d245e066b43b2a7cf9426a18ab4f84
Olivier Gauthé <olivier.gauthe@epfl.ch>
2021-05-11 12:08:45 +0200

    define separate get_transfer_matrix

--- ctmrg/observables.py
+++ ctmrg/observables.py
@@ -2,7 +2,11 @@ import numpy as np
 import scipy.sparse.linalg as slg
 
 
-def compute_corr_length(Tup_list, Tdown_list, v0=None, ncv=None, maxiter=1000, tol=0):
+def get_transfer_matrix(Tup_list, Tdown_list):
+    """
+    Construct environment transfer matrix as a scipy LinearOperator, with on the fly
+    matrix product.
+    """
     assert len(Tup_list) == len(Tdown_list), "Tup_list != Tdown_list"
     sh_vec = (Tup_list[0].shape[3], Tdown_list[0].shape[3])
     sh_op = (sh_vec[0] * sh_vec[1],) * 2
@@ -16,7 +20,11 @@ def compute_corr_length(Tup_list, Tdown_list, v0=None, ncv=None, maxiter=1000, t
         return y.ravel()
 
     op = slg.LinearOperator(sh_op, matvec=transfer_mat_dot)
+    return op
 
+
+def compute_corr_length(Tup_list, Tdown_list, v0=None, ncv=None, maxiter=1000, tol=0):
+    op = get_transfer_matrix(Tup_list, Tdown_list)
     try:
         vals = slg.eigs(
             op, k=2, v0=v0, ncv=ncv, maxiter=maxiter, tol=tol, return_eigenvectors=False
