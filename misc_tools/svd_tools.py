99fc9b1953b088e58b5d73c8538b8054ca3b8001
Olivier Gauthé <olivier.gauthe@epfl.ch>
2021-10-17 12:42:52 +0200

    clean svd, remove unused function

--- misc_tools/svd_tools.py
+++ misc_tools/svd_tools.py
@@ -118,12 +118,13 @@ def svd_truncate(
     Note that U(1) symmetry forces to compute much more than k vectors, hence a small
     or even 0 window is fine.
     """
+    # DO NOT USE
+    # keep this function until refactoring simple_update with SymmetricTensor
+
     if full or min(M.shape) < 3 * cut:  # full allows to use numba while cutting
         U, s, V, colors = svdU1(M, row_colors, col_colors)
-    else:
-        U, s, V, colors = sparse_svdU1(  # FIXME: interplay svd_truncate / sparse_svdU1
-            M, cut + window, row_colors, col_colors, maxiter=maxiter
-        )
+    else:  # not used in simple update
+        raise NotImplementedError
     # zeros must be removed, without caring for multiplets. If a non-zero multiplet is
     # split, cutoff was not set properly.
     cut = min(cut, (s > cutoff * s[0]).nonzero()[0][-1] + 1)
@@ -144,7 +145,6 @@ def sparse_svd(A, k=6, ncv=None, tol=0, maxiter=None, return_singular_vectors=Tr
     # compute either no singular vectors or both U and V
     # ref: https://github.com/scipy/scipy/pull/11829
 
-    A = np.asarray(A)  # do not consider LinearOperator or sparse matrix
     n, m = A.shape
     dmin = min(m, n)
 
@@ -162,11 +162,8 @@ def sparse_svd(A, k=6, ncv=None, tol=0, maxiter=None, return_singular_vectors=Tr
     def dot_XH_X(x):
         return XH_dot(X_dot(x))
 
-    XH_X = slg.LinearOperator(
-        matvec=dot_XH_X, matmat=dot_XH_X, shape=(dmin, dmin), dtype=A.dtype
-    )
-
     # lobpcg is not reliable, see scipy issue #10974.
+    XH_X = slg.LinearOperator(matvec=dot_XH_X, shape=(dmin, dmin), dtype=A.dtype)
     eigvals, eigvec = slg.eigsh(XH_X, k=k, tol=tol, maxiter=maxiter, ncv=ncv)
 
     # improve stability following https://github.com/scipy/scipy/pull/11829
@@ -183,117 +180,3 @@ def sparse_svd(A, k=6, ncv=None, tol=0, maxiter=None, return_singular_vectors=Tr
     else:
         vh = vh @ eigvec.T.conj()
     return u, s, vh
-
-
-# use of eigsh forbids numba
-def sparse_svdU1(M, k_block, row_colors, col_colors, maxiter=1000):
-    """
-    *** SEVERAL PROBLEMS IN THIS FUNCTION. NOT USED CURRENTLY ***
-
-    Compute a given number of singular values and singular vectors in each U(1) symmetry
-    sector. If colors are not provided, fall back to standard sparse SVD, with only one
-    block.
-
-    Parameters
-    ----------
-    M : (m,n) ndarray
-      Matrix to decompose.
-    k_block : integer
-      Number of singular vectors to compute for each block.
-    row_colors : (m,) integer ndarray
-      U(1) quantum numbers of the rows.
-    col_colors : (n,) integer ndarray
-      U(1) quantum numbers of the columns.
-    maxiter : integer
-      Maximal number of Arpack iterations. Finite by default allows clean crash
-      instead of running forever.
-
-    Returns
-    -------
-    U :   U : (m,cut) ndarray
-      Left singular vectors.
-    s : (cut,) ndarray
-      Singular values.
-    V : (cut,n) right singular vectors
-    color : (cut,) ndarray with int8 data type
-      U(1) quantum numbers of s.
-    """
-    raise NotImplementedError
-    # FIXME: dealing with cut, blockwise or global?
-    # computing k_block values by block without cutting is clumsy.
-    # matrix element sorting at the end is inefficient if cut is made later
-    # how to deal with multiplets? Should be done in svd_truncate
-    # probably need to inline this in svd_truncate and restrain this function to
-    # computation of k largest values, staying close to sparse_svd.
-
-    # revert to standard sparse svd if colors are not provided
-    if not row_colors.size or not col_colors.size:
-        U, s, V = sparse_svd(M, k=k_block, maxiter=maxiter)
-        return U, s, V, default_color
-
-    if M.shape != (row_colors.shape, col_colors.shape):
-        raise ValueError("Colors do not match M")
-
-    # optimize cache in matrix block reduction with stable sort
-    row_sort = row_colors.argsort(kind="stable")
-    sorted_row_colors = row_colors[row_sort]
-    col_sort = col_colors.argsort(kind="stable")
-    sorted_col_colors = col_colors[col_sort]
-    row_blocks = np.array(
-        [
-            0,
-            *((sorted_row_colors[:-1] != sorted_row_colors[1:]).nonzero()[0] + 1),
-            M.shape[0],
-        ]
-    )
-    col_blocks = [
-        0,
-        *((sorted_col_colors[:-1] != sorted_col_colors[1:]).nonzero()[0] + 1),
-        M.shape[1],
-    ]
-
-    # we need to compute k_block singular values in every sector to deal with the worst
-    # case: every k_block largest values in the same sector.
-    # A fully memory efficient code would only store 2*cut vectors. Here we select the
-    # cut largest values selection once all vector are computed, so we need to store
-    # sum( min(cut, sector_size) for each sector) different vectors.
-    # Dealing with different block numbers in row_blocks and col_blocks here is
-    # cumbersome, so we consider the sub-optimal min(cut, sector_rows_number) while
-    # columns number may be smaller.
-    max_k = np.minimum(k_block, row_blocks[1:] - row_blocks[:-1]).sum()
-    U = np.zeros((M.shape[0], max_k))
-    S = np.empty(max_k)
-    V = np.zeros((max_k, M.shape[1]))
-    colors = np.empty(max_k, dtype=np.int8)
-
-    # match blocks with same color and compute SVD inside those blocks only
-    k, rbi, cbi, rbimax, cbimax = 0, 0, 0, row_blocks.size - 1, len(col_blocks) - 1
-    while rbi < rbimax and cbi < cbimax:
-        if sorted_row_colors[row_blocks[rbi]] == sorted_col_colors[col_blocks[cbi]]:
-            ri = row_sort[row_blocks[rbi] : row_blocks[rbi + 1]]
-            ci = col_sort[col_blocks[cbi] : col_blocks[cbi + 1]]
-            m = np.ascontiguousarray(M[ri[:, None], ci])  # no numba
-            if min(m.shape) < 3 * k_block:  # use exact svd for small blocks
-                u, s, v = lg.svd(m, full_matrices=False, overwrite_a=True)
-            else:
-                u, s, v = sparse_svd(m, k=k_block, maxiter=maxiter)
-
-            d = min(k_block, s.size)  # may be smaller than k_block
-            U[ri, k : k + d] = u[:, :d]
-            S[k : k + d] = s[:d]
-            V[k : k + d, ci] = v[:d]
-            colors[k : k + d] = sorted_row_colors[row_blocks[rbi]]
-            k += d
-            rbi += 1
-            cbi += 1
-        elif sorted_row_colors[row_blocks[rbi]] < sorted_col_colors[col_blocks[cbi]]:
-            rbi += 1
-        else:
-            cbi += 1
-
-    s_sort = S[:k].argsort()[::-1]  # k <= max_k
-    S = S[s_sort]
-    U = U[:, s_sort]
-    V = V[s_sort]
-    colors = colors[s_sort]
-    return U, S, V, colors
