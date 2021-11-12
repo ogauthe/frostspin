59ae71bfd1f96a8fe26dd2d65be45f1e0227882d
Olivier Gauthé <olivier.gauthe@epfl.ch>
2021-11-12 21:12:43 +0100

    WIP

--- symmetric_tensor/non_abelian_symmetric_tensor.py
+++ symmetric_tensor/non_abelian_symmetric_tensor.py
@@ -1,3 +1,5 @@
+import bisect
+
 import numpy as np
 import scipy.linalg as lg
 
@@ -10,6 +12,59 @@ class NonAbelianSymmetricTensor(SymmetricTensor):
     """
 
     _unitary_dic = NotImplemented
+
+    def construct_unitary(self, new_row_reps, new_col_reps, axes):
+        r"""
+        Construct isometry corresponding to change of non-abelian tensor tree structure.
+
+                    initial
+                    /      \
+                   /\      /\
+                  /\ \    / /\
+                  rows1    columns1
+                  |||      |||
+                 transposition
+                  ||||      ||
+                  rows2     columns2
+                 \/ / /     \  /
+                  \/ /       \/
+                   \/        /
+                    \       /
+                      output
+
+        Parameters
+        ----------
+        new_row_reps: tuple of representations
+            Representation of transposed tensor row legs
+        new_col_reps: tuple of representations
+            Representation of transposed tensor column legs
+        axes: tuple of int
+            axes permutation
+        """
+        proj1 = self.construct_matrix_projector(self._row_reps, self._col_reps)
+        proj2 = self.construct_matrix_projector(new_row_reps, new_col_reps)
+
+        # so, now we have initial shape projector and output shape projector. We need to
+        # transpose rows to contract them. Since there is no bra/ket exchange, this can
+        # be done by pure advanced slicing, without calling heavier sparse_transpose.
+        nsh = tuple(
+            self.representation_dimension(rep) for rep in new_row_reps + new_col_reps
+        )
+        strides1 = np.array((1,) + self._shape[:0:-1]).cumprod()[::-1]
+        strides2 = np.array((1,) + nsh[:0:-1]).cumprod()[::-1]
+        perm = (np.arange(proj1.shape[0])[:, None] // strides1 % self._shape)[
+            :, axes
+        ] @ strides2
+
+        proj2 = proj2[perm].T.tocsr()
+        unitary = proj2 @ proj1
+        # tests show that construct_matrix_projector output has no numerical zeros
+        # however unitary may have more than 70% stored coeff coeff being numerical
+        # zeros, with several order of magnitude between them and real non-zeros.
+        unitary.data[np.abs(unitary.data) < 1e-14] = 0.0
+        unitary.eliminate_zeros()
+        unitary = unitary.sorted_indices()  # copy to get clean data array
+        return unitary
 
     @classmethod
     def construct_matrix_projector(row_reps, col_reps, conjugate_columns=True):
@@ -38,6 +93,7 @@ class NonAbelianSymmetricTensor(SymmetricTensor):
 
     @classmethod
     def from_raw_data(cls, raw_data, row_reps, col_reps):
+        # TODO put sqrt(dim) in "unitary" and jit me
         row_rep = cls.combine_representations(*row_reps)
         col_rep = cls.combine_representations(*col_reps)
         i1 = 0
@@ -47,17 +103,18 @@ class NonAbelianSymmetricTensor(SymmetricTensor):
         k = 0
         while i1 < row_rep.shape[1] and i2 < col_rep.shape[1]:
             if row_rep[1, i1] == col_rep[1, i2]:
-                sh = (row_rep[1, i1], col_rep[1, i2])
+                sh = (row_rep[0, i1], col_rep[0, i2])
                 m = raw_data[k : k + sh[0] * sh[1]].reshape(sh)
                 k += m.size
-                blocks.append(m / np.sqrt(cls.representation_dimension(row_rep[0, i1])))
-                block_irreps.append(row_rep[0, i1])
+                blocks.append(m / np.sqrt(cls.irrep_dimension(row_rep[1, i1])))
+                block_irreps.append(row_rep[1, i1])
                 i1 += 1
                 i2 += 1
-            elif row_rep[0, i1] < col_rep[0, i1]:
+            elif row_rep[1, i1] < col_rep[1, i2]:
                 i1 += 1
             else:
                 i2 += 1
+        block_irreps = np.array(block_irreps)
         return cls(row_reps, col_reps, blocks, block_irreps)
 
     @classmethod
@@ -80,16 +137,39 @@ class NonAbelianSymmetricTensor(SymmetricTensor):
             arr = self.T.toarray()
             k = len(self._col_reps)
             return arr.transpose(tuple(range(k, self._ndim)) + tuple(range(k)))
-        proj, ind = self.construct_matrix_projector(
-            self._row_reps, self._col_reps, conjugate_columns=True
-        )
-        m = np.zeros((np.prod(self._shape),), dtype=self.dtype)
-        m[ind] = proj @ self.to_raw_data()
+        proj = self.construct_matrix_projector(self._row_reps, self._col_reps)
+        arr = proj @ self.to_raw_data()
         if as_matrix:
-            return m
-        return m.reshape(self.shape)
+            return arr.reshape(self.matrix_shape)
+        return arr.reshape(self.shape)
+
+    def to_raw_data(self):
+        # some blocks may be missing, yet raw_data has to include the corresponding
+        # zeros at the accurate position. This is inefficient.
+        # TODO: remove this method
+        row_rep = self.get_row_representation()
+        col_rep = self.get_column_representation()
+        shared, indL, indR = np.intersect1d(  # bruteforce numpy > clever python
+            row_rep[1],
+            col_rep[1],
+            assume_unique=True,
+            return_indices=True,
+        )
+        data = np.zeros(row_rep[0, indL] @ col_rep[0, indR])
+        k = 0
+        # hard to jit, need to call self._blocks[i] which may be heterogenous
+        for i, irr in enumerate(shared):
+            j = bisect.bisect_left(self._block_irreps, irr)
+            if j < self._nblocks and self._block_irreps[j] == irr:
+                b = self._blocks[j]
+                data[k : k + b.size] = b.ravel() * np.sqrt(self.irrep_dimension(irr))
+                k += b.size
+            else:  # missing block
+                k += row_rep[0, indL[i]] * col_rep[0, indR[i]]
+        return data
 
     def permutate(self, row_axes, col_axes):
+        assert sorted(row_axes + col_axes) == list(range(self._ndim))
         nx0 = len(self._row_reps)
         row_reps = []
         for ax in row_axes:
@@ -106,13 +186,22 @@ class NonAbelianSymmetricTensor(SymmetricTensor):
                 col_reps.append(self._col_reps[ax - nx0])
         col_reps = tuple(col_reps)
 
-        k = (tuple(r.tobytes() for r in row_reps), tuple(r.tobytes() for r in col_reps))
-        unitary = self._unitary_dic[k]
+        # if hash is too slow, can be decomposed: at init, set dic corresponding to
+        # representations, then permutate only needs hash from row_axes and col_axes
+        key = tuple(r.tobytes() for r in self._row_reps + self._col_reps)
+        key = key + (len(self._row_reps), row_axes, col_axes)
+        try:
+            unitary = self._unitary_dic[key]
+        except KeyError:
+            unitary = self.construct_unitary(row_axes, col_axes, row_axes + col_axes)
+            self._unitary_dic[key] = unitary
+        # TODO slice unitary to do product blockwise, allowing for missing blocks
+        # also include sqrt(dim) in input and output
         raw_data = unitary @ self.to_raw_data()
         return self.from_raw_data(raw_data, row_reps, col_reps)
 
     def norm(self):
         n2 = 0.0
         for (irr, b) in zip(self._block_irreps, self._blocks):
-            n2 += self.representation_dimension(irr) * lg.norm(b) ** 2
+            n2 += self.irrep_dimension(irr) * lg.norm(b) ** 2
         return np.sqrt(n2)
--- symmetric_tensor/su2_symmetric_tensor.py
+++ symmetric_tensor/su2_symmetric_tensor.py
@@ -1,23 +1,38 @@
+import numpy as np
 import numba
 
 from symmetric_tensor.non_abelian_symmetric_tensor import NonAbelianSymmetricTensor
+from groups.su2_representation import SU2_Representation  # TODO remove me
+import groups.su2_matrix  # TODO remove me
+
+
+@numba.njit
+def _numba_elementary_combine_SU2(r1, r2):
+    degen = np.zeros((r1[1, -1] + r2[1, -1] - 1,), dtype=np.int64)
+    for i1 in range(r1.shape[1]):
+        for i2 in range(r2.shape[1]):
+            for irr in range(abs(r1[1, i1] - r2[1, i2]), r1[1, i1] + r2[1, i2] - 1, 2):
+                degen[irr] += r1[0, i1] * r2[0, i2]  # shit irr-1 <-- irr
+    nnz = degen.nonzero()[0]
+    return np.concatenate((degen[nnz], nnz + 1)).reshape(2, -1)
 
 
 @numba.njit
 def _numba_combine_SU2(*reps):
     combined = reps[0]
     for r in reps[1:]:
-        combined = combined
+        combined = _numba_elementary_combine_SU2(combined, r)
     return combined
 
 
 class SU2_SymmetricTensor(NonAbelianSymmetricTensor):
     """
-    Irreps are 2D arrays with np.uint16 dtype. First row is degen, second row is irrep
+    Irreps are 2D arrays with int dtype. First row is degen, second row is irrep
     dimension = 2 * s + 1
     """
 
     _symmetry = "SU(2)"
+    _unitary_dic = {}
 
     @classmethod
     def combine_representations(cls, *reps):
@@ -33,5 +48,19 @@ class SU2_SymmetricTensor(NonAbelianSymmetricTensor):
     def representation_dimension(cls, rep):
         return rep[0] @ rep[1]
 
+    @classmethod
+    def irrep_dimension(cls, irr):
+        return irr
+
     def group_conjugated(self):
         return self  # all SU(2) representations are self-conjugate
+
+    @classmethod
+    def construct_matrix_projector(cls, row_reps, col_reps, conjugate_columns=True):
+        # WIP
+        # reuse SU2_Matrix stuff
+        rep_left_enum = tuple(SU2_Representation(r[0], r[1]) for r in row_reps)
+        rep_right_enum = tuple(SU2_Representation(r[0], r[1]) for r in col_reps)
+        return groups.su2_matrix.construct_matrix_projector(
+            rep_left_enum, rep_right_enum, conj_right=conjugate_columns
+        )
