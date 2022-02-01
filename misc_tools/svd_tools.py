16dedaa763f1ab9fb2dd30c273d4103782354c6c
Olivier Gauthé <olivier.gauthe@epfl.ch>
2022-02-01 13:50:55 +0100

    rm toolsU1, SU2_SimpleUpdate and SU2_Matrix, clean svd

--- misc_tools/svd_tools.py
+++ misc_tools/svd_tools.py
@@ -3,8 +3,6 @@ import scipy.linalg as lg
 import scipy.sparse.linalg as slg
 import numba
 
-from groups.toolsU1 import default_color, svdU1
-
 
 def find_chi_largest(block_s, chi, dims, rcutoff=0.0, degen_ratio=1.0):
     """
@@ -58,86 +56,6 @@ def _numba_find_chi_largest(block_s, chi, dims, rcutoff, degen_ratio):
             bi = block_max_vals.argmax()
 
     return block_cuts
-
-
-def svd_truncate(
-    M,
-    cut,
-    row_colors=default_color,
-    col_colors=default_color,
-    full=False,
-    maxiter=1000,
-    cutoff=0.0,
-    degen_ratio=1.0,
-    window=0,
-):
-    """
-    Unique function to compute singular value decomposition of a matrix and truncate.
-    If row_colors and col_colors are provided, use U(1) symmetry to compute blockwise
-    SVD in every color sectors. Matrices are smaller, but more vectors are computed.
-
-    Parameters
-    ----------
-    M : (m,n) ndarray
-      Matrix to decompose.
-    cut : integer
-      Number of singular vectors to keep. Actual value depends on kept multiplets.
-    row_colors : (m,) integer ndarray
-      U(1) quantum numbers of the rows.
-    col_colors : (n,) integer ndarray
-      U(1) quantum numbers of the columns.
-    full : boolean
-      Whether to compute the full SVD of the matrix (avoid calling sparse SVD function)
-    maxiter : integer
-      Maximal number of arpack iterations. Finite by default allows clean crash instead
-      of running forever. Not read if full is True.
-    cutoff : float
-      Singular values smaller than cutoff * max(singular values) are set to zero and
-      associated singular vectors are removed. Default is 0.0 (no cutoff)
-    degen_ratio : float
-        Used to define multiplets in singular values and truncate between two
-        multiplets. Two consecutive singular values are considered degenerate if
-        1 >= s[i+1]/s[i] >= degen_ratio > 0. Default is 1.0 (exact degeneracies)
-    window : integer
-      If degen_ratio is not None and full is false, compute cut + window vectors in each
-      sector to preserve multiplets. Default is 0.
-
-    Returns
-    -------
-    U : (m, k) ndarray
-      Left singular vectors.
-    s : (k,) ndarray, float
-      Singular values.
-    V : (k, n) ndarray
-      Right singular vectors.
-    colors : (k,) ndarray, int8
-      U(1) quantum numbers of s.
-
-    Notes:
-    ------
-    Even if a truncature is made, full can be used to compute the full SVD, which may
-    be more precise or faster (especially with numba)
-    Note that U(1) symmetry forces to compute much more than k vectors, hence a small
-    or even 0 window is fine.
-    """
-    # DO NOT USE
-    # keep this function until refactoring simple_update with SymmetricTensor
-
-    if full or min(M.shape) < 3 * cut:  # full allows to use numba while cutting
-        U, s, V, colors = svdU1(M, row_colors, col_colors)
-    else:  # not used in simple update
-        raise NotImplementedError
-    # zeros must be removed, without caring for multiplets. If a non-zero multiplet is
-    # split, cutoff was not set properly.
-    cut = min(cut, (s > cutoff * s[0]).nonzero()[0][-1] + 1)
-    nnz = (s[cut:] <= degen_ratio * s[cut - 1 : -1]).nonzero()[0]
-    if nnz.size:
-        cut += nnz[0]
-    U = U[:, :cut]
-    s = s[:cut]
-    V = V[:cut]
-    colors = colors[:cut]
-    return U, s, V, colors
 
 
 def sparse_svd(A, k=6, ncv=None, tol=0, maxiter=None, return_singular_vectors=True):
