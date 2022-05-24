1018425171b3657a4f8b956d09d1ca3d4156c218
Olivier Gauthé <olivier.gauthe@epfl.ch>
2022-05-24 15:53:08 +0200

    add signature

--- ctmrg/ctm_renormalize.py
+++ ctmrg/ctm_renormalize.py
@@ -130,8 +130,11 @@ def construct_projectors(
 
     block_irreps = corner2.block_irreps[ind2[non_empty]]
     mid_rep = corner2.init_representation(block_cuts[non_empty], block_irreps)
-    P = type(corner2)((mid_rep,), corner2.col_reps, p_blocks, block_irreps).T
-    Pt = type(corner2)(corner2.col_reps, (mid_rep,), pt_blocks, block_irreps)
+    s2 = corner2.signature[corner2.n_row_reps :]
+    sp = np.hstack((np.array([False]), s2))
+    spt = np.hstack((~s2, np.array([True])))
+    P = type(corner2)((mid_rep,), corner2.col_reps, p_blocks, block_irreps, sp).T
+    Pt = type(corner2)(corner2.col_reps, (mid_rep,), pt_blocks, block_irreps, spt)
     return P, Pt
 
 
