ca0270b7dea88c058f2029444493988a069ff869
Olivier Gauthé <olivier.gauthe@epfl.ch>
2022-08-01 11:08:23 +0200

    remove einsum

--- symmetric_tensor/o2_symmetric_tensor.py
+++ symmetric_tensor/o2_symmetric_tensor.py
@@ -439,7 +439,7 @@ class O2_SymmetricTensor(NonAbelianSymmetricTensor):
 
             rso = rszb_mat.argsort().argsort()
             cso = cszb_mat.argsort().argsort()
-            bsign = np.einsum("i,j,ij->ij", rsign, csign, self._blocks[isz])
+            bsign = rsign[:, None] * self._blocks[isz] * csign
             b = _numba_generate_block(rso, cso, bsign)
             blocks.append(b)
             isz -= 1
