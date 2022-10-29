6ba8f540a31753cd7e7085cdc898daca4e28a458
Olivier Gauthé <olivier.gauthe@epfl.ch>
2022-10-29 20:41:16 +0200

    numba way for combine_U1

--- symmetric_tensor/u1_symmetric_tensor.py
+++ symmetric_tensor/u1_symmetric_tensor.py
@@ -7,19 +7,16 @@ from .abelian_symmetric_tensor import AbelianSymmetricTensor
 @numba.njit(parallel=True)
 def _numba_combine_U1(reps, signature):
     nx = len(reps)
-    signs = (1 - 2 * signature).astype(np.int8)
-    mod = np.array([r.size for r in reps], dtype=np.uint64)
-    strides = np.ones((nx,), dtype=np.uint64)
-    strides[1:] = mod[-1:0:-1]
-    strides = strides.cumprod()[::-1].copy()
-    combined = np.empty((strides[0] * mod[0],), dtype=np.int8)
-    for i in numba.prange(combined.size):
-        sz = 0
-        for j in range(nx):
-            ind = i // strides[j] % mod[j]
-            sz += signs[j] * reps[j][ind]
-        combined[i] = sz
-    return combined
+    c0 = np.int8(1 - 2 * signature[0]) * reps[0]
+    for i in range(1, nx):
+        n = reps[i].size
+        rs = (1 - 2 * signature[i]) * reps[i]
+        c1 = np.empty((c0.size * n,), dtype=np.int8)
+        for j in numba.prange(c0.size):
+            for k in range(n):
+                c1[j * n + k] = c0[j] + rs[k]
+        c0 = c1
+    return c0
 
 
 class U1_SymmetricTensor(AbelianSymmetricTensor):
@@ -41,7 +38,7 @@ class U1_SymmetricTensor(AbelianSymmetricTensor):
     @staticmethod
     def combine_representations(reps, signature):
         assert signature.shape == (len(reps),)
-        if len(reps) > 1:
+        if len(reps) > 1:  # numba issue 7245
             return _numba_combine_U1(tuple(reps), signature)
         return (1 - 2 * signature[0]) * reps[0]
 
