bb8ba422aae1e164b5fbd8ebffd388e817e94604
Olivier Gauthé <ogauthe@flatironinstitute.org>
2024-01-22 18:49:31 -0500

    define eigs with on the fly matmat

--- symmetric_tensor/tools.py
+++ symmetric_tensor/tools.py
@@ -1,3 +1,8 @@
+import numpy as np
+import scipy.linalg as lg
+import scipy.sparse.linalg as slg
+
+from misc_tools.svd_tools import find_chi_largest
 from .asymmetric_tensor import AsymmetricTensor
 from .u1_symmetric_tensor import U1_SymmetricTensor
 from .o2_symmetric_tensor import O2_SymmetricTensor
@@ -26,3 +31,141 @@ def get_symmetric_tensor_type(symmetry):
     except KeyError:
         raise ValueError(f"Unknown symmetry '{symmetry}'")
     return st_type
+
+
+def symmetric_sparse_eigs(
+    ST,
+    reps,
+    sig0,
+    nvals,
+    matmat,
+    dmax_full=100,
+    rng=None,
+    maxiter=4000,
+    tol=0,
+    return_dense=True,
+):
+    """
+    Find nvals eigenvalues for an implicit square matrix defined by matmat.
+
+    Parameters
+    ----------
+    ST : type
+        Symmetry tensor type of the implicit matrix M.
+    reps : enumerable of representations
+        Row representations for M
+    sig0 : bool 1D array
+        Signature for M rows
+    nvals : int
+        Number of eigenvalues to compute
+    matmat : callable
+        Apply matrix A to a symmetric tensor
+    dmax_full : int
+        Maximum block size to use dense eigvals.
+    rng : numpy random generator
+        Random number generator.
+    maxiter : int
+        Maxiter for Arpack.
+    tol : float
+        Arpack tol.
+    return_dense : bool
+        Whether to return a dense numpy array or a list of sector wise values. Default
+        is True.
+
+    Returns
+    -------
+    if return_dense:
+        vals : 1D complex array
+            Computed eigenvalues, as a dense array with multiplicites, sorted by
+            decreasing absolute value. The last multiplet is cut to return exactly
+            nvals values.
+    else:
+        vals : tuple of 1D complex array
+            Eigenvalues for each non empty block, sorted by decreasing absolute value.
+        block_irreps : int ndarray
+            Irrep for each kept block
+    """
+    if rng is None:
+        rng = np.random.default_rng(42)
+
+    n = len(reps)
+    sigm = np.empty((2 * n,), dtype=bool)
+    sigm[:n] = sig0
+    sigm[n:] = ~sig0
+    block_irreps, block_shapes = ST.get_block_sizes(reps, reps, sigm)
+    assert all(block_shapes[:, 0] == block_shapes[:, 1])
+
+    nblocks = block_shapes.shape[0]
+    sparse = []
+    full = []
+    blocks = []
+    dims = np.empty((nblocks,), dtype=int)
+    for bi in range(nblocks):
+        irr = block_irreps[bi]
+        dims[bi] = ST.irrep_dimension(irr)
+        if block_shapes[bi, 0] > max(dmax_full, 2 * nvals / dims[bi]):
+            sparse.append(bi)
+        else:
+            full.append(bi)
+            blocks.append(np.eye(block_shapes[bi, 0]))
+
+    sigv = np.ones((n + 1,), dtype=bool)
+    sigv[:-1] = sig0
+    ev_blocks = [None] * nblocks
+    abs_ev_blocks = [None] * nblocks
+
+    if full:
+        irr_full = np.ascontiguousarray(block_irreps[full])
+        rfull = ST.init_representation(block_shapes[full, 0], irr_full)
+        full_blocks = ST(reps, (rfull,), blocks, irr_full, sigv)
+        full_blocks = matmat(full_blocks)
+        for i, bi in enumerate(full):
+            ev = lg.eigvals(full_blocks.blocks[i])
+            abs_ev = np.abs(ev)
+            so = abs_ev.argsort()[::-1]
+            ev_blocks[bi] = ev[so]
+            abs_ev_blocks[bi] = abs_ev[so]
+
+    for bi in sparse:
+        irr = block_irreps[bi : bi + 1]
+        brep = ST.init_representation(np.ones((1,), dtype=int), irr)
+        sh = block_shapes[bi]
+
+        def matvec(x):
+            st = ST(reps, (brep,), (x[:, None],), irr, sigv)
+            st = matmat(st)
+            i = st.block_irreps.searchsorted(irr)[0]
+            return st.blocks[i].ravel()
+
+        op = slg.LinearOperator(sh, matvec=matvec)
+        v0 = rng.random((sh[0],)) - 0.5
+        k = nvals // dims[bi] + 1
+        ev = slg.eigs(
+            op, k=k, v0=v0, maxiter=maxiter, tol=tol, return_eigenvectors=False
+        )
+        abs_ev = np.abs(ev)
+        so = abs_ev.argsort()[::-1]
+        ev_blocks[bi] = ev[so]
+        abs_ev_blocks[bi] = abs_ev[so]
+
+    cuts = find_chi_largest(abs_ev_blocks, nvals, dims=dims)
+
+    if return_dense:
+        vals = np.empty((cuts @ dims,), dtype=np.complex128)
+        k = 0
+        for bi in range(nblocks):
+            bv = ev_blocks[bi][: cuts[bi]]
+            for d in range(dims[bi]):
+                vals[k : k + cuts[bi]] = bv
+                k += cuts[bi]
+
+        so = np.argsort(np.abs(vals))[-1 : -nvals - 1 : -1]
+        vals = np.ascontiguousarray(vals[so])
+        return vals
+
+    non_empty = cuts.nonzero()[0]
+    block_irreps = np.ascontiguousarray(block_irreps[non_empty])
+    final_ev = [None] * non_empty.size
+    for i, bi in enumerate(non_empty):
+        final_ev[i] = ev_blocks[bi][: cuts[bi]]
+    return tuple(final_ev), block_irreps
---
+++ tests/test_eigs.py
@@ -0,0 +1,60 @@
+#!/usr/bin/env python
+
+import numpy as np
+import scipy.linalg as lg
+
+import symmetric_tensor.tools
+
+
+ST_SU2 = symmetric_tensor.tools.get_symmetric_tensor_type("SU2")
+rng = np.random.default_rng(42)
+reps = (np.array([[2, 3, 1], [1, 3, 5]]), np.array([[2, 2, 1], [1, 3, 5]]))
+mat_su2 = ST_SU2.random(reps, reps, rng=rng)
+mat_su2 /= mat_su2.norm()
+mat_o2 = mat_su2.toO2()
+mat_u1 = mat_su2.toU1()
+mat_as = mat_su2.totrivial()
+dense = mat_su2.toarray(as_matrix=True)
+
+default = lg.eigvals(dense)
+default = default[np.abs(default).argsort()[::-1]]
+
+
+nvals = 20
+dmax_full = 10
+
+
+def get_vals(mat, dmax_full):
+    ST = type(mat)
+    sig0 = mat.signature[: mat.n_row_reps]
+    vals = symmetric_tensor.tools.symmetric_sparse_eigs(
+        ST, mat.row_reps, sig0, nvals, lambda st: mat @ st, rng=rng, dmax_full=dmax_full
+    )
+    return vals
+
+
+vsu2 = get_vals(mat_su2, dmax_full)
+vo2 = get_vals(mat_o2, dmax_full)
+vu1 = get_vals(mat_u1, dmax_full)
+vas = get_vals(mat_as, dmax_full)
+
+
+# due to degen, actual sizes may be larger than nvals
+d1 = default[:nvals]
+d2 = d1.conj()
+
+assert all((np.abs(d1 - vsu2) < 1e-12) | (np.abs(d2 - vsu2) < 1e-12))
+assert all((np.abs(d1 - vo2) < 1e-12) | (np.abs(d2 - vo2) < 1e-12))
+assert all((np.abs(d1 - vu1) < 1e-12) | (np.abs(d2 - vu1) < 1e-12))
+assert all((np.abs(d1 - vas) < 1e-12) | (np.abs(d2 - vas) < 1e-12))
+
+vals, irreps = symmetric_tensor.tools.symmetric_sparse_eigs(
+    ST_SU2,
+    mat_su2.row_reps,
+    mat_su2.signature[:2],
+    nvals,
+    lambda st: mat_su2 @ st,
+    rng=rng,
+    dmax_full=dmax_full,
+    return_dense=False,
+)
